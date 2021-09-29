0aa145bb5c2673aba50009fceed5ac7202b56e7e
LucasSouusa <72485990+LucasSouusa@users.noreply.github.com>
2021-09-28 21:32:55 -0300

    Atividade Impar e par

---
+++ Node.js
@@ -0,0 +1,21 @@
+const aluno = 10;
+
+for(var cont=0 ; cont <= aluno; cont++){
+ 
+    console.log(cont);
+
+    if(cont == 0){
+    
+    console.log("É zero");
+    }
+
+   else if(cont %2 ==0){
+    
+    console.log("É Par");
+     
+}   else
+    
+    console.log("É Ímpar");
+
+
+}
